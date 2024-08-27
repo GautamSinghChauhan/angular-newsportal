ead8885153260ae4a0c807a9a11d37ca8331e7c4
unknown <singhchauhangautam@gmail.com>
2024-08-27 22:57:25 +0530

    parent to chil data send

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 
 @Component({
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+
+  @Input() headerTitle!: string;
 
   constructor() { }
 
